b81031337e17354fc0d8358a0b046aa27976bd3b
FlorianKomnino <florian.komnino@outlook.it>
2022-11-30 17:31:12 +0100

    added console.log() of clicked number

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log('Hello World!');
 let gameArea = document.getElementById('gameArea');
 
 //funzione che crea un elemento html
-function getElementDiv (contentText, whereWillGo) {
+function getElementDiv (contentText, whereWillGo, numberToPrint) {
     let createdElement = document.createElement('div');
     // il testo contenuto all'interno dell'elemento é l'argomento inserito
     createdElement.innerText = contentText;
@@ -13,6 +13,7 @@ function getElementDiv (contentText, whereWillGo) {
     whereWillGo.append(createdElement);
     createdElement.addEventListener ('click', function() {
         createdElement.classList.toggle('bgAlternativo');
+        console.log(numberToPrint)
     })
     return createdElement;
 }
@@ -26,6 +27,6 @@ playButton.addEventListener ('click', function() {
     gameArea.innerHTML = '';
     // ciclo che esegue la funzione getElementDiv 100 volte, inserendo ogni volta il numero dell'interazione come testo dell'elemento inserito
     for ( let i = 1 ; i < 101 ; i++) {
-        getElementDiv(i, gameArea);
+        getElementDiv(i, gameArea, i);
     }
 })
