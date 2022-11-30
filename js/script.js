a9f0210fccc6c1b9aa0568633806bb14847e602f
FlorianKomnino <florian.komnino@outlook.it>
2022-11-30 15:50:31 +0100

    added first element to gameArea

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 console.log('Hello World!');
 
+const createdElement = document.createElement('p');
 
+const gameArea = document.getElementById('gameArea');
+
+console.log(gameArea);
+
+gameArea.append(createdElement);
