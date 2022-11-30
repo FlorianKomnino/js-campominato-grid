034476ba6a2cb24d6a8fb476671e04173f9d34a5
FlorianKomnino <florian.komnino@outlook.it>
2022-11-30 17:18:22 +0100

    added function property to empty gameArea.innerHTML when button is pushed. Changed variable gameArea in the function, now is one of the two arguments

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
 console.log('Hello World!');
 
-const gameArea = document.getElementById('gameArea');
+let gameArea = document.getElementById('gameArea');
 
 //funzione che crea un elemento html
-function getElementDiv (contentText) {
+function getElementDiv (contentText, whereWillGo) {
     let createdElement = document.createElement('div');
     // il testo contenuto all'interno dell'elemento é l'argomento inserito
     createdElement.innerText = contentText;
     // aggiungo classi casella e p-2
     createdElement.classList.add('casella', 'p-2');
     // appendo l'elemento alla variabile gameArea
-    gameArea.append(createdElement);
+    whereWillGo.append(createdElement);
     return createdElement;
 }
 
-
-// ciclo che esegue la funzione getElementDiv 100 volte, inserendo ogni volta il numero dell'interazione come testo dell'elemento inserito
-for ( let i = 1 ; i < 101 ; i++) {
-    getElementDiv(i);
-}
-
-
-
-
-
-
+//variabile assegnata all'id del playButton
 let playButton = document.getElementById('playButton');
+
+// event listener che permette l'interazione con il playButton
+playButton.addEventListener ('click', function() {
+    gameArea.innerHTML = '';
+    // ciclo che esegue la funzione getElementDiv 100 volte, inserendo ogni volta il numero dell'interazione come testo dell'elemento inserito
+    for ( let i = 1 ; i < 101 ; i++) {
+        getElementDiv(i, gameArea);
+    }
+})
