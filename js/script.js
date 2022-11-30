c6d4be8ec6e74d208af5a906a2eafc976e139460
FlorianKomnino <florian.komnino@outlook.it>
2022-11-30 16:51:54 +0100

    added comments

--- js/script.js
+++ js/script.js
@@ -2,14 +2,19 @@ console.log('Hello World!');
 
 const gameArea = document.getElementById('gameArea');
 
+//funzione che crea un elemento html
 function getElementDiv (contentText) {
     let createdElement = document.createElement('div');
+    // il testo contenuto all'interno dell'elemento é l'argomento inserito
     createdElement.innerText = contentText;
+    // aggiungo classi casella e p-2
     createdElement.classList.add('casella', 'p-2');
+    // appendo l'elemento alla variabile gameArea
     gameArea.append(createdElement);
     return createdElement;
 }
 
+// ciclo che esegue la funzione getElementDiv 100 volte, inserendo ogni volta il numero dell'interazione come testo dell'elemento inserito
 for ( let i = 1 ; i < 101 ; i++) {
     getElementDiv(i);
 }
